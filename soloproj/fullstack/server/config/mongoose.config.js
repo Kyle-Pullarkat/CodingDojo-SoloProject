012e96dd4dd8f0109126f26c73035a3a13b13eed
Kyle-Pullarkat <kpullarkat@yahoo.com>
2022-08-14 14:39:37 -0700

    del

--- soloproj/fullstack/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoEndpoint = 'mongodb://localhost/';
-const dbName = 'products3';
-
-mongoose
-    .connect(mongoEndpoint + dbName)
-    .then(() => console.log(`Connected to ${dbName} database`))
-    .catch((err) => console.log('Error in connection to DB', err));
